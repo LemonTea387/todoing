95503c06b9fb52316fcc9c5a02ad1e99451de6d9
LemonTea387 <junjielow08@gmail.com>
2024-02-16 13:37:01 +0800

    Normalize db

--- migrations/20240215101425_tasks.sql
+++ migrations/20240215101425_tasks.sql
@@ -3,7 +3,16 @@ CREATE TABLE IF NOT EXISTS tasks
   id          INTEGER PRIMARY KEY NOT NULL,
   title       TEXT NOT NULL,
   description TEXT,
-  status      BLOB NOT NULL,
-  priority    BLOB,
+  priority    INTEGER,
   created_dt  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tasks_activity 
+(
+  activity_id INTEGER PRIMARY KEY,
+  status      INTEGER NOT NULL,
+  started_dt  DATETIME,
+  ended_dt    DATETIME,
+  task_id     INTEGER NOT NULL,
+  FOREIGN KEY(task_id) REFERENCES tasks(id)
 );
